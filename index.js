8c967e14c426b6beda30dfa4de2ae322250d0630
Prosper <prosperorjiogo1999@gmail.com>
2025-02-03 08:48:02 +0100

    [state]: initialize project with Express setup, add README and .gitignore

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    const response = {
+        email: 'plance991@gmail.com',
+        currentdate: new Date().toISOString(),
+        github_url: 'https:github.com/tempahh/hngint'
+    };
+
+    res.json(response);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}
